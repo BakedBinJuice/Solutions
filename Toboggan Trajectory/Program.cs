c70fb3a278bde27774318c7ff87b90ab0570c1e8
Alex Hammond <bingly101@gmail.com>
2020-12-19 15:01:57 +1000

    Dodge the trees.
    This problem has a given input of trees that you have to count each tree that you hit with the movements of right 3 down 1. When you reach the end of the line instead of multiplying the data and taking up heaps of space, you mod the column position by the row length to wrap to the start of the next row.

---
+++ Toboggan Trajectory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toboggan_Trajectory
+{
+    class Trajectory
+    {
+        public void trajectory()
+        {
+            var readData = System.IO.File.ReadAllLines(@"data.txt");
+            int treeCount = 0;
+            var width = readData[0].Length;
+            int colIndex = 0;
+            for (int rowIndex = 0; rowIndex < readData.Length; rowIndex++)
+            {
+                if (readData[rowIndex][colIndex % width] == '#')
+                {
+                    treeCount++;
+                }
+                colIndex += 3;
+            }
+            Console.WriteLine(treeCount);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trajectory Trajectory = new Trajectory();
+            Trajectory.trajectory();
+        }
+    }
+}
