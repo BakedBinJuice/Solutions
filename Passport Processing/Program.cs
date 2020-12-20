866f10aca01a7602869bc916d0d55090b816fb62
Alex Hammond <bingly101@gmail.com>
2020-12-20 23:21:47 +1000

    Passport processing problem part 1
    This is the solution to the following problem.
    byr (Birth Year)
    iyr (Issue Year)
    eyr (Expiration Year)
    hgt (Height)
    hcl (Hair Color)
    ecl (Eye Color)
    pid (Passport ID)
    cid (Country ID)
    Passport data is validated in batch files (your puzzle input). Each passport is represented as a sequence of key:value pairs separated by spaces or newlines. Passports are separated by blank lines.
    
    Here is an example batch file containing four passports:
    
    ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
    byr:1937 iyr:2017 cid:147 hgt:183cm
    
    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
    hcl:#cfa07d byr:1929
    
    hcl:#ae17e1 iyr:2013
    eyr:2024
    ecl:brn pid:760753108 byr:1931
    hgt:179cm
    
    hcl:#cfa07d eyr:2025 pid:166559648
    iyr:2011 ecl:brn hgt:59in
    The first passport is valid - all eight fields are present. The second passport is invalid - it is missing hgt (the Height field).
    
    The third passport is interesting; the only missing field is cid, so it looks like data from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made the system temporarily ignore missing cid fields. Treat this "passport" as valid.
    
    The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing any other field is not, so this passport is invalid.
    
    According to the above rules, your improved system would report 2 valid passports.
    
    Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?

---
+++ Passport Processing/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Passport_Processing
+{
+    class Processing
+    {
+        public void PassportProcessing()
+        {
+            var readData = System.IO.File.ReadAllLines(@"data.txt");
+            string[] criterea = { "byr", "iry", "eyr", "hgt", "hcl", "ecl", "pid", "cid" };
+            int count = 0;
+            List<string> instanceGroup = new List<string>();
+            for (int i = 0; i < readData.Length; i++)
+            {
+                string readLine = readData[i];
+                char[] dataArray = readData[i].ToCharArray();
+                if (readLine != "")
+                {
+                    for (int n = 0; n < readLine.Length; n++)
+                    {
+                        if (readLine[n].Equals(':'))
+                        {
+                            int x = n - 3;
+                            int y = n - 2;
+                            int z = n - 1;
+                            string instance = readLine[x].ToString() + readLine[y].ToString() + readLine[z].ToString();
+                            instanceGroup.Add(instance);
+                        }
+                    }
+                }
+                else
+                {
+                    if (instanceGroup.Count == criterea.Length)
+                    {
+                        count++;
+                        //Console.WriteLine(count);
+                    }
+                    else if (instanceGroup.Count == 7)
+                    {
+                        if (instanceGroup.Contains("cid") == false)
+                        {
+                            count++;
+                            //Console.WriteLine(count);
+                        }
+                    }
+                    instanceGroup.Clear();
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processing Processing = new Processing();
+            Processing.PassportProcessing();
+        }
+    }
+}
