9e8e51a044e4036108aa69f870f71519aac978fe
Alex Hammond <bingly101@gmail.com>
2021-01-07 09:25:59 +1000

    starting day 5 advent of code

---
+++ Binary Boarding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Binary_Boarding
+{
+    class BinaryBoarding
+    {
+        public void part1()
+        {
+            string input = "FBFBBFFRLR";
+            int range = 127;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'F')
+                {
+                    range = range / 2;
+                    Console.WriteLine("lower " + "0 through " + range);
+                }
+                else if (input[i] == 'B')
+                {
+                    int x = range;
+                    range = range / 2;
+                    Console.WriteLine("upper " + range + " though " + x);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryBoarding binaryBoarding = new BinaryBoarding();
+            binaryBoarding.part1();
+        }
+    }
+}
