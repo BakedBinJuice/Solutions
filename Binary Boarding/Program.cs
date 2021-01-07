8a4078851e6ee8f520735a049f671a8cc9e54f51
Alex Hammond <bingly101@gmail.com>
2021-01-08 08:25:43 +1000

    Almost finished writing the algo!

--- Binary Boarding/Program.cs
+++ Binary Boarding/Program.cs
@@ -7,10 +7,25 @@ namespace Binary_Boarding
         public void part1()
         {
             string input = "FBFBBFFRLR";
-            int range = 127;
+            double startRange = 0;
+            double endRange = 127;
+
             for (int i = 0; i < input.Length; i++)
             {
-                
+                for (double x = 0; x < endRange; x = x + 0.1)
+                {
+                    startRange += 0.1;
+                    endRange -= 0.1;
+                    int startMid = (int)Math.Round(startRange);
+                    int  endMid = (int)Math.Round(endRange);
+                    //Console.WriteLine(startRange + " " + endRange);
+                    if (startMid == endMid)
+                    {
+                        Console.WriteLine(startMid + " " + endMid);
+                        break;
+                    }
+                }
+                break;
             }
         }
     }
