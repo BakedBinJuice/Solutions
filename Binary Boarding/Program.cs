204d20807af9c1f43ae478efdf90982506d4a26a
Alex Hammond <bingly101@gmail.com>
2021-01-09 09:24:26 +1000

    almost finished the algorigthm to find the middle number!

--- Binary Boarding/Program.cs
+++ Binary Boarding/Program.cs
@@ -4,28 +4,46 @@ namespace Binary_Boarding
 {
     class BinaryBoarding
     {
+        public decimal findMidNum(Char lu, double z, double y) 
+        {
+            decimal finalInt = 0;
+            for (double x = 0; x < y; x = x + 0.1)
+            {
+                z += 0.1;
+                y -= 0.1;
+                if (z + 0.5 > y)
+                {
+                    decimal startMid = Math.Floor((decimal)z);
+                    decimal endMid = Math.Ceiling((decimal)y);
+                    if (lu == 'F')
+                    {
+                        finalInt = startMid;
+                        return finalInt;
+                    }
+                    else if (lu == 'B')
+                    {
+                        finalInt = endMid;
+                        return finalInt;
+                    }
+                }
+            }
+            return finalInt;
+
+        }
         public void part1()
         {
             string input = "FBFBBFFRLR";
             double startRange = 0;
-            double endRange = 127;
-
+            double val = 127;
+            decimal valDec = 127;
+            double staticStartRange = 0;
+            double staticEndRange = 127;
             for (int i = 0; i < input.Length; i++)
             {
-                for (double x = 0; x < endRange; x = x + 0.1)
-                {
-                    startRange += 0.1;
-                    endRange -= 0.1;
-                    int startMid = (int)Math.Round(startRange);
-                    int  endMid = (int)Math.Round(endRange);
-                    //Console.WriteLine(startRange + " " + endRange);
-                    if (startMid == endMid)
-                    {
-                        Console.WriteLine(startMid + " " + endMid);
-                        break;
-                    }
-                }
-                break;
+                val = System.Convert.ToDouble(valDec);
+                decimal foundNum = findMidNum(input[i], startRange, val);
+                valDec = System.Convert.ToDecimal(foundNum);
+                Console.WriteLine(val);
             }
         }
     }
